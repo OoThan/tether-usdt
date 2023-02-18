3eccc61f5846b1c862a09a5dcbbba7e59c2b1c9f
maroo <maroo@gmail.com>
2023-02-18 10:13:04 +0630

    add multi signature wallet sol file

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -134,8 +134,237 @@ contract MultiSigWallet {
                 owners[i] = owners[owners.length - 1];
             }
         owners.length -= 1;
-//        if (required > owners.length)
+        if (required > owners.length)
+            changeRequirement (owners.length);
+        emit OwnerAddition (owner);
+    }
 
-            
+    /**
+     *  @dev Allows to replace an owner with a new owner. Transaction has to be sent by wallet.
+     *  @param owner Address of owner to be replaced.
+     *  @param owner Address of new owner.
+     */
+    function replaceOwner (address owner, address newOwner)
+        public
+        onlyWallet
+        ownerExists (owner)
+        ownerDoesNotExist (newOwner)
+    {
+        for (uint i = 0; i <owners.length; i++)
+            if (owners[i] == owner) {
+                owners[i] = newOwner;
+                break;
+            }
+        isOwner[owner] = false;
+        isOwner[newOwner] = true;
+        emit OwnerRemoval (owner);
+        emit OwnerAddition (newOwner);
+    }
+
+    /**
+     *  @dev allows to change the number of required confirmations. Transaction has to be sent by wallet.
+     *  @param _required Number of required confirmations.
+     */
+    function changeRequirement (uint _required)
+        public
+        onlyWallet
+        validRequirement (owners.length, _required)
+    {
+        required = _required;
+        emit RequirementChange (_required);
+    }
+
+    /**
+     *  @dev Allows an owner to submit and confirm a transaction
+     *  @param destination Transaction target address
+     *  @param value Transaction ether value
+     *  @param data Transaction data payload
+     *  @return Returns Transaction ID
+     */
+    function submitTransaction (address destination, uint value, bytes data)
+        public
+        returns (uint transactionId)
+    {
+        transactionId = addTransaction (destination, value, data);
+        confirmTransaction (transactionId);
+    }
+
+    /**
+     *  @dev Allows an owner to confirm a transaction.
+     *  @param Returns transaction ID
+     */
+    function confirmTransaction (uint transactionId)
+        public
+        ownerExists (msg.sender)
+        transactionExists (transactionId)
+        notConfirmed (transactionId, msg.sender)
+    {
+        confirmations[transactionId][msg.sender] = true;
+        emit Confirmation (msg.sender, transactionId);
+        executeTransaction (transactionId);
+    }
+
+    /**
+     *  @dev Allows to an owner to revoke a confirmation for a transaction
+     *  @param transactionId Transaction ID
+     */
+    function revokeConfirmation (uint transactionId)
+        public
+        ownerExists (msg.sender)
+        confirmed (transactionId, msg.sender)
+        notExecuted (transactionId)
+    {
+        confirmations[transactionId][msg.sender] = true;
+        emit Confirmation (msg.sender, transactionId);
+        executeTransaction (transactionId);
+    }
+
+    /**
+     *  @dev Allows anyone to execute a confirmed transaction
+     *  @param transactionId Transaction ID
+     */
+    function executeTransaction (uint transactionId)
+        public
+        notExecuted (transactionId)
+    {
+        confirmations[transactionId][msg.sender] = false;
+        emit Revocation (msg.sender, transactionId);
+    }
+
+    /**
+     *  @dev Returns the confirmation status of a transaction
+     *  @param transactionId Transaction ID
+     *  @return Confirmation status
+     */
+    function isConfirmed (uint transactionId)
+        public
+        constant
+        returns (bool)
+    {
+        uint count = 0;
+        for (uint i = 0; i < owners.length; i++) {
+            if (confirmations[transactionId][owners[i]])
+                count ++;
+            if (count == required)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     *  @title Internal Functions
+     *  @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+     *  @param destination Transaction target address
+     *  @param value Transaction ether value
+     *  @param data Transaction data payload
+     *  @return Returns transaction ID
+     */
+    function addTransaction (address destination, uint value, bytes data)
+        internal
+        notNull (destination)
+        returns (uint transactionId)
+    {
+        transactionId = transactionCount;
+        transactions[transactionId] = Transaction({
+            destination: destination,
+            value: value,
+            data: data,
+            executed: false
+        });
+        transactionCount ++;
+        emit Submission (transactionId);
+    }
+
+    /**
+     *  @title Web3 call function
+     *  @dev Return the number of confirmations of a transaction
+     *  @param transactionId Transaction ID
+     *  @return Number of confirmations
+     */
+    function getConfirmationCount (uint transactionId)
+        public
+        constant
+        returns (uint count)
+    {
+        for (uint i = 0; i < owners.length; i++)
+            if (confirmations[transactionId][owners[i]])
+                count ++;
+    }
+
+    /**
+     *  @dev Returns the total number of transactions after filters are applied
+     *  @param pending Include pending transactions
+     *  @param executed Include executed transactions
+     *  @return Total number of transactions after filters are applied
+     */
+    function getTransactionCount (bool pending, bool executed)
+        public
+        constant
+        returns (uint count)
+    {
+        for (uint i = 0; i < transactionCount; i++)
+            if (pending && !transactions[i].executed || executed && transactions[i].executed)
+                count ++;
+    }
+
+    /**
+     *  @dev Return list of owners
+     *  @return List of owner address
+     */
+    function getOwners ()
+        public
+        constant
+        returns (address[])
+    {
+        return owners;
+    }
+
+    /**
+     *  @dev Return array with owner address, which confirmed transaction
+     *  @param transactionId Transaction ID
+     *  @return Returns the array of owner addresses
+     */
+    function getConfirmations (uint transactionId)
+        public
+        constant
+        returns (address[] _confirmations)
+    {
+        address[] memory confirmationsTemp = new address[](owners.length);
+        uint count = 0;
+        uint i;
+        for (i = 0; i < owners.length; i++)
+            if (confirmations[transactionId][owners[i]]){
+                confirmationsTemp[count] = owners[i];
+                count ++;
+            }
+        _confirmations = new address[](count);
+        for (i = 0; i < count; i++)
+            _confirmations[i] = confirmationsTemp[i];
+    }
+
+    /**
+     *  @dev Returns the list of transaction IDs in defined range.
+     *  @param from Index start position of transaction array
+     *  @param to Index end position of transaction array
+     *  @param pending Include pending transactions.
+     *  @param executed Include executed transactions.
+     *  @return Returns array of transaction IDs.
+     */
+    function getTransactionIds (uint from, uint to, bool pending, bool executed)
+        public
+        constant
+        returns (uint[] _transactionIds)
+    {
+        uint[] memory transactionIdsTemp = new uint[](transactionCount);
+        uint count = 0;
+        uint i;
+        for (i = 0; i < transactionCount; i++)
+            if (pending && !transactions[i].executed || executed && transactions[i].executed) {
+                transactionIdsTemp[count] = i;
+                count ++;
+            }
+        _transactionIds = new uint[](to - from);
+        for (i = from; i < to; i++)
+            _transactionIds[i - from] = transactionIdsTemp[i];
     }
 }
